6ac0dc6a6649820f62066ee2197e632021f295a0
sewelarosina <kseakamela08@gmail.com>
2026-02-04 11:50:59 +0200

    added appointments class and doctors claa along with their index an create cshtml and updated a code in each class

---
+++ HospitalApp/Controllers/AppointmentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+public class AppointmentsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AppointmentsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var appointments = _context.Appointments
+            .Select(a => new
+            {
+                a.Id,
+                Patient = _context.Patients.FirstOrDefault(p => p.Id == a.PatientId),
+                Doctor = _context.Doctors.FirstOrDefault(d => d.Id == a.DoctorId),
+                a.AppointmentDate
+            })
+            .ToList();
+
+        return View(appointments);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Patients = _context.Patients.ToList();
+        ViewBag.Doctors = _context.Doctors.ToList();
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Appointment appointment)
+    {
+        _context.Appointments.Add(appointment);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
---
+++ HospitalApp/Controllers/DoctorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class DoctorsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public DoctorsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        if (!_context.Doctors.Any())
+        {
+            _context.Doctors.AddRange(
+                new Doctor { FullName = "Dr Smith", Specialty = "General" },
+                new Doctor { FullName = "Dr Ndlovu", Specialty = "Pediatrics" }
+            );
+            _context.SaveChanges();
+        }
+
+        return View(_context.Doctors.ToList());
+    }
+}
