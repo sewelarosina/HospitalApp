69808b105ed32f69800cdbbefba1e9612379a4bc
sewelarosina <kseakamela08@gmail.com>
2026-02-04 11:21:25 +0200

    created a data folder and added an applicationDbcontext class in the data folder

---
+++ HospitalApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+}
