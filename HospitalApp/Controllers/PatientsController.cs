c046294aa8037748583e23cc7725cca48a646382
sewelarosina <kseakamela08@gmail.com>
2026-02-04 11:29:54 +0200

    added a patientcontroller in controller folder

---
+++ HospitalApp/Controllers/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class PatientsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public PatientsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Patients.ToList());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Patient patient)
+    {
+        _context.Patients.Add(patient);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
