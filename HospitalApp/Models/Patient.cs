9876e40dd64793abe4f095da421b3b92f0ebd038
sewelarosina <kseakamela08@gmail.com>
2026-02-04 11:11:31 +0200

    Created amodel folser and added a patient class and updated a code

---
+++ HospitalApp/Models/Patient.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+}
